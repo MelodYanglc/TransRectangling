bc435d4524151af2a0c346b09c5134d0063301b0
MelodYanglc <150918173+MelodYanglc@users.noreply.github.com>
2024-06-28 09:34:03 +0800

    Update constant.py

--- utils/constant.py
+++ utils/constant.py
@@ -1,4 +1,5 @@
 # define the mesh resolution
+# Directly increasing the number of meshes leads to significant performance gains. But considering the fairness. We keep the same mesh size setting as YuDeepRectangling.
 GRID_W = 8
 GRID_H = 6
 GPU_DEVICE = "cuda:0"
